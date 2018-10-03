8843ccac5d49f434c788f37b969e881e3134447e
JackReis01 <jackreis2019@gmail.com>
2018-10-03 08:59:18 -0500

    updated text

--- Chapter-8/src/practice/DemoArray.java
+++ Chapter-8/src/practice/DemoArray.java
@@ -6,12 +6,15 @@ public class DemoArray {
 		// TODO Auto-generated method stub
 		
 		double[] salaries = {6.25, 6.55, 10.25, 16.85};
+		double total = 0;
 		
 		System.out.println("Salaries one by one are: ");
 		
 		for(int i = 0; i < salaries.length; i = i + 1)
 		{
 			System.out.println(salaries[i]);
+			
+			total = total + salaries[i];
 		}
 
 	}
