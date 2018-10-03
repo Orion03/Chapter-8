2dcd3dc775f5a575baf93af424fc6c78023a0158
JackReis01 <jackreis2019@gmail.com>
2018-10-03 08:57:49 -0500

    updated text

--- Chapter-8/src/practice/DemoArray.java
+++ Chapter-8/src/practice/DemoArray.java
@@ -5,20 +5,11 @@ public class DemoArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double[] salaries = new double[4];
-		
-		salaries[0] = 6.25;
-		salaries[1] = 6.55;
-		salaries[2] = 10.25;
-		salaries[3] = 16.85;
+		double[] salaries = {6.25, 6.55, 10.25, 16.85};
 		
 		System.out.println("Salaries one by one are: ");
-		System.out.println(salaries[0]);
-		System.out.println(salaries[1]);
-		System.out.println(salaries[2]);
-		System.out.println(salaries[3]);
 		
-		for(int i = 0; i < salaries.length; i = i +1)
+		for(int i = 0; i < salaries.length; i = i + 1)
 		{
 			System.out.println(salaries[i]);
 		}
