4de6fb8c08585fe6341080dbb5186efafb7a9e6c
JackReis01 <jackreis2019@gmail.com>
2018-10-03 08:51:30 -0500

    updated text

---
+++ Chapter-8/src/practice/DemoArray.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class DemoArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double[] salaries = new double[4];
+		
+		
+
+	}
+
+}
