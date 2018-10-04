fb6d4ac248e70b11384410dbe63f79f9069bfa0a
JackReis01 <jackreis2019@gmail.com>
2018-10-04 09:10:26 -0500

    updated text

--- Chapter-8/src/exercises/ArrayMethodDemo.java
+++ Chapter-8/src/exercises/ArrayMethodDemo.java
@@ -1,9 +1,20 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ArrayMethodDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int[] heldIntegers = new int[10];
+		
+		for(int i = 0; i > heldIntegers.length; i++) {
+			System.out.println("Enter 10 integers >> ");
+			heldIntegers[i] = input.nextInt();
+		}
 
 	}
 
